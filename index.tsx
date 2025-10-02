848aef2204648196c9f49f582f7a2923653ae45e
fanmutta <137640329+fanmutta@users.noreply.github.com>
2025-10-02 14:06:51 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // atau komponen utama Anda
+import './index.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
